5320f5f6556952df31eb178db7f8afbfdcb13308
lado2008 <adamialado133@gmail.com>
2025-03-11 19:38:49 +0400

    Made Bace Structure of Website

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
 import './style.css'
 
-console.log
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/public/data/planets.json") 
+        .then(response => response.json())
+        .then(planets => {
+            const navLinks = document.querySelectorAll(".navbar ul li a")
+            const planetName = document.querySelector(".planet_name")
+            const planetImage = document.querySelector(".planet_image")
+
+            navLinks.forEach(link => {
+                link.addEventListener("click", function (event) {
+                    event.preventDefault()
+
+                    const selectedPlanet = planets.find(planet => planet.name.toLowerCase() === link.textContent.toLowerCase())
+
+                    if (selectedPlanet) {
+                        planetName.textContent = selectedPlanet.name.toUpperCase()
+                        planetImage.src = selectedPlanet.image
+                        planetImage.alt = selectedPlanet.name
+                    }
+                });
+            });
+        })
+        .catch(error => console.error("Error loading planets:", error))
+});
+
+
